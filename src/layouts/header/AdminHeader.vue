72d2c10b84a62accaaae17338d3c5afbad6a6f67
orzorzooo <orzorzooo@gmail.com>
2022-10-20 15:45:09 +0800

    EDIT: AdminHeader RWD樣式

--- src/layouts/header/AdminHeader.vue
+++ src/layouts/header/AdminHeader.vue
@@ -1,26 +1,57 @@
 <template>
   <a-layout-header :class="[headerTheme, 'admin-header']">
-    <div :class="['admin-header-wide', layout, pageWidth]">
-      <router-link v-if="isMobile || layout === 'head'" to="/" :class="['logo', isMobile ? null : 'pc', headerTheme]">
-        <img width="32" src="@/assets/img/logo.png" />
+    <div
+      :class="[
+        'admin-header-wide',
+        layout,
+        pageWidth,
+        'flex justify-between !static',
+      ]"
+    >
+      <router-link
+        v-if="isMobile || layout === 'head'"
+        to="/"
+        :class="['logo', isMobile ? null : 'pc', headerTheme]"
+      >
+        <!-- <img width="32" src="@/assets/img/logo.png" /> -->
         <h1 v-if="!isMobile">{{ systemName }}</h1>
       </router-link>
-      <a-divider v-if="isMobile" type="vertical" />
+      <!-- <a-divider v-if="isMobile" type="vertical" /> -->
       <a-icon
-        v-if="layout !== 'head'"
+        v-if="layout !== 'head' && !isMobile"
         class="trigger"
         :type="collapsed ? 'menu-unfold' : 'menu-fold'"
         @click="toggleCollapse"
       />
-      <div v-if="layout !== 'side' && !isMobile" class="admin-header-menu" :style="`width: ${menuWidth};`">
-        <i-menu class="head-menu" :theme="headerTheme" mode="horizontal" :options="menuData" @select="onSelect" />
+
+      <div
+        v-if="layout !== 'side' && !isMobile"
+        class="admin-header-menu"
+        :style="`width: ${menuWidth};`"
+      >
+        <i-menu
+          class="head-menu"
+          :theme="headerTheme"
+          mode="horizontal"
+          :options="menuData"
+          @select="onSelect"
+        />
       </div>
-      <div :class="['admin-header-right', headerTheme]">
+      <div :class="['admin-header-right', headerTheme, 'flex justify-between']">
         <!-- 搜尋框 -->
-        <header-search v-if="show.search" class="header-item" @active="(val) => (searchActive = val)" />
+        <header-search
+          v-if="show.search"
+          class="header-item"
+          @active="(val) => (searchActive = val)"
+        />
 
         <!-- 操作文檔 -->
-        <a-tooltip class="header-item" title="帮助文档" placement="bottom" v-if="show.tooltip">
+        <a-tooltip
+          class="header-item"
+          title="帮助文档"
+          placement="bottom"
+          v-if="show.tooltip"
+        >
           <a href="https://iczer.gitee.io/vue-antd-admin-docs/" target="_blank">
             <a-icon type="question-circle-o" />
           </a>
@@ -35,7 +66,11 @@
         <!-- 語言設定 -->
         <a-dropdown class="lang header-item" v-if="show.language">
           <div><a-icon type="global" /> {{ langAlias }}</div>
-          <a-menu @click="(val) => setLang(val.key)" :selected-keys="[lang]" slot="overlay">
+          <a-menu
+            @click="(val) => setLang(val.key)"
+            :selected-keys="[lang]"
+            slot="overlay"
+          >
             <a-menu-item v-for="lang in langList" :key="lang.key">{{
               lang.key.toLowerCase() + " " + lang.name
             }}</a-menu-item>
@@ -74,9 +109,20 @@ export default {
     };
   },
   computed: {
-    ...mapState("setting", ["theme", "isMobile", "layout", "systemName", "lang", "pageWidth"]),
+    ...mapState("setting", [
+      "theme",
+      "isMobile",
+      "layout",
+      "systemName",
+      "lang",
+      "pageWidth",
+    ]),
     headerTheme() {
-      if (this.layout == "side" && this.theme.mode == "dark" && !this.isMobile) {
+      if (
+        this.layout == "side" &&
+        this.theme.mode == "dark" &&
+        !this.isMobile
+      ) {
         return "light";
       }
       return this.theme.mode;
@@ -106,4 +152,8 @@ export default {
 
 <style lang="less" scoped>
 @import "index";
+@purple-500: #7e4efc;
+.admin-header.dark {
+  background-color: @purple-500;
+}
 </style>
