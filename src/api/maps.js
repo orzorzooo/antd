1ed0b88c6867c9249ae9efe3e053ad64ba61d359
orzorzooo <orzorzooo@gmail.com>
2022-10-24 17:04:58 +0800

    EDIT: 串接國土資源及nominatim

--- src/api/maps.js
+++ src/api/maps.js
@@ -1,4 +1,6 @@
 import { request, api, BASEURL } from "@/utils/request";
+import axios from "axios";
+export const URL = `${BASEURL}/map`;
 
 const SEARCH_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/`;
 const ACCESS_TOKEN =
@@ -7,3 +9,15 @@ const ACCESS_TOKEN =
 export const findAll = (locate) =>
   request(`${SEARCH_URL}${locate}.json?access_token=${ACCESS_TOKEN}`, "get");
 // https://api.mapbox.com/geocoding/v5/mapbox.places23.json?access_token=pk.eyJ1Ijoib3J6b3J6b29vIiwiYSI6ImNsOWh1dXpjdTVxeDgzdm9pa2cweG1raHUifQ.WEEOxYk0SqsysjOuOjUTmg
+
+export const nominatim = (locate) =>
+  axios.get("https://nominatim.openstreetmap.org/search", {
+    params: { q: locate, format: "json" },
+  });
+
+// export const nisc_bus = ({ lon = 120, lat = 20, radius = 100 }) =>
+//   axios.get(
+//     `https://api.nlsc.gov.tw/other/MarkBufferAnlys/bus/${lon}/${lat}/${radius}`
+//   );
+export const nisc_bus = ({ lon = 120, lat = 20, radius = 100 }) =>
+  api.get(`${URL}/nisc/bus`, { lon, lat, radius });
--- src/pages/maps/index.vue
+++ src/pages/maps/index.vue
@@ -26,14 +26,13 @@
 <script>
 import Mapbox from "mapbox-gl";
 import { MglMap } from "vue-mapbox";
-import { findAll } from "@/api/maps";
+import { findAll, nominatim, nisc_bus } from "@/api/maps";
 import axios from "axios";
 const ACCESS_TOKEN =
   "pk.eyJ1Ijoib3J6b3J6b29vIiwiYSI6ImNsOWh1dXpjdTVxeDgzdm9pa2cweG1raHUifQ.WEEOxYk0SqsysjOuOjUTmg";
-const MAP_STYLE = "mapbox://styles/orzorzooo/cl9hxghyh000614pj3k07al3w";
+const MAP_STYLE = "mapbox://styles/orzorzooo/cl9mf86c1001e16pow4fu38qt";
 // const searchURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/San%20Francisco.json?access_token=pk.eyJ1Ijoib3J6b3J6b29vIiwiYSI6ImNsOWh1dXpjdTVxeDgzdm9pa2cweG1raHUifQ.WEEOxYk0SqsysjOuOjUTmg`;
-const SEARCH_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/
-`;
+const SEARCH_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/ `;
 export default {
   components: {
     MglMap,
@@ -67,18 +66,38 @@ export default {
         speed: 2,
       });
     },
-    async search() {
-      const url = `${SEARCH_URL}` + this.locate + `.json`;
-      console.log(url);
-      const { data } = await axios.get(url, {
-        params: {
-          access_token: ACCESS_TOKEN,
-          // proximity: `120.232905,22.9903046`,
-        },
+    async getBUS({ lon, lat, radius }) {
+      console.log(lon, lat);
+      const { data } = await nisc_bus({
+        lon: Number(lon),
+        lat: Number(lat),
+        radius,
       });
-      this.map.flyTo({ center: data.features[0].center });
-      this.shop = await this.searchGEO(data.features[0].center);
+      return data;
+    },
+    async search() {
+      const { data } = await nominatim(this.locate);
       console.log(data);
+      const result = data[0];
+      this.map.flyTo({ center: [result.lon, result.lat] });
+      const busDatas = await this.getBUS({
+        lon: result.lon,
+        lat: result.lat,
+        radius: 100,
+      });
+      console.log("fuck", busDatas);
+
+      // const url = `${SEARCH_URL}` + this.locate + `.json`;
+      // console.log(url);
+      // const { data } = await axios.get(url, {
+      //   params: {
+      //     access_token: ACCESS_TOKEN,
+      //     // proximity: `120.232905,22.9903046`,
+      //   },
+      // });
+      // this.map.flyTo({ center: data.features[0].center });
+      // this.shop = await this.searchGEO(data.features[0].center);
+      // console.log(data);
     },
     async searchGEO(coordinate) {
       const url = `${SEARCH_URL}` + `7-11` + `.json`;
